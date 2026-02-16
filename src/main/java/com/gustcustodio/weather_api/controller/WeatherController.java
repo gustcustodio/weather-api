70819d16632e8bb79a7e3a376c317c387beef635
gustcustodio <gustcustodio@gmail.com>
2026-02-16 09:23:22 -0300

    create WeatherController

---
+++ src/main/java/com/gustcustodio/weather_api/controller/WeatherController.java
@@ -0,0 +1,25 @@
+package com.gustcustodio.weather_api.controller;
+
+import com.gustcustodio.weather_api.entity.WeatherResponse;
+import com.gustcustodio.weather_api.service.WeatherService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/weather")
+public class WeatherController {
+
+    private WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping("/{city}")
+    public WeatherResponse getWeather(@PathVariable String city) {
+        return weatherService.getWeather(city);
+    }
+
+}
